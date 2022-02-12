9c416e3f3cab6cd49def174664ab16d5114cfc32
bhuwan <bkspoudel@gmail.com>
2022-02-12 09:31:42 +0100

    solved the bug

---
+++ src/components/HeaderContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import TextInput from "./TextInput";
+
+const HeaderContainer = () => {
+  return (
+    <div>
+      <h1>hello from headercontainer...</h1>
+      <TextInput />
+    </div>
+  );
+};
+
+export default HeaderContainer;
