33e63e21b68e52c918ad6c830a22f0a4784e7c87
bhuwan <bkspoudel@gmail.com>
2022-02-12 08:37:48 +0100

    created a TodoPage and imported in App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import "./App.css";
 import Counter from "./Counter";
+import TodoPage from "./TodoPage";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <Counter count={count} handleCount={handleCount} />
+      <TodoPage />
     </div>
   );
 }
