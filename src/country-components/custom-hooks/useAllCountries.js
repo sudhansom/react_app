be2c806037ff8b15916c8af2614cef288e493dca
bhuwan <bkspoudel@gmail.com>
2022-02-15 20:37:46 +0100

    changed the url parameter

--- src/country-components/custom-hooks/useAllCountries.js
+++ src/country-components/custom-hooks/useAllCountries.js
@@ -1,22 +1,22 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const useAllCountries = (allCountries_url) => {
+const useAllCountries = (url) => {
   const [data, setData] = useState([]);
   const [err, setError] = useState();
 
   useEffect(() => {
     try {
-      const fetchCountries = async (allCountries_url) => {
-        const result = await axios.get(allCountries_url);
+      const fetchCountries = async (url) => {
+        const result = await axios.get(url);
         console.log("in custom hook:", result);
         setData(result.data);
       };
-      fetchCountries(allCountries_url);
+      fetchCountries(url);
     } catch (err) {
       setError(err);
     }
-  }, [allCountries_url]);
+  }, [url]);
 
   return [err, data];
 };
--- src/country-components/custom-hooks/useOneCountry.js
+++ src/country-components/custom-hooks/useOneCountry.js
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const useOneCountry = (oneCountry_url) => {
+const useOneCountry = (url) => {
   const [data, setData] = useState([]);
   const [err, setError] = useState();
 
   useEffect(() => {
     try {
-      const fetchCountries = async (oneCountry_url) => {
-        const result = await axios.get(oneCountry_url);
+      const fetchCountries = async (url) => {
+        const result = await axios.get(url);
         console.log("in custom hook one Country:", result);
         setData(result.data[0]);
       };
-      fetchCountries(oneCountry_url);
+      fetchCountries(url);
     } catch (err) {
       setError(err);
     }
