f505fde3c48649072baefe282ceab0afff58e3cd
bhuwan <bkspoudel@gmail.com>
2022-02-16 09:39:16 +0100

    imported Provider and store to index.js and wrapped App with them

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
 import "./index.css";
 import App from "./App";
+
 //import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
+import storeRedux from "./components/redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={storeRedux}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
